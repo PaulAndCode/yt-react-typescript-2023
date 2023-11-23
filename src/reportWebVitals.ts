a4076db340694d6155f1249cf11f13f0e5214740
Paul Liu <oucl5976@gmail.com>
2023-11-23 19:25:54 +0000

    Fix ESLint errors

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,15 +1,17 @@
-import { ReportHandler } from 'web-vitals';
+import { type ReportHandler } from 'web-vitals'
 
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
+const reportWebVitals = (onPerfEntry?: ReportHandler): void => {
+  if (onPerfEntry != null && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+      getCLS(onPerfEntry)
+      getFID(onPerfEntry)
+      getFCP(onPerfEntry)
+      getLCP(onPerfEntry)
+      getTTFB(onPerfEntry)
+    }).catch((ex): void => {
+      console.error(ex)
+    })
   }
-};
+}
 
-export default reportWebVitals;
+export default reportWebVitals
